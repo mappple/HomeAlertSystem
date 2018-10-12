30ea1579c351a10a01b76292c31d279ed0a09932
mappple <mappple@qq.com>
2018-10-12 18:23:34 +1100

    Initial Commit

---
+++ HomeAlertSystem/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HomeAlertSystem
+//
+//  Created by Mappple on 12/10/18.
+//  Copyright © 2018 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
